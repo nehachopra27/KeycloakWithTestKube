d06395423e7feb950c8aa8558c8f7a9ffcdf8ae6
nchopra <nchopra@redhat.com>
2022-10-18 16:35:18 +0100

    Adding few tests

--- src/test/java/com/keycloak/test/LaunchTest.java
+++ src/test/java/com/keycloak/test/LaunchTest.java
@@ -1,17 +1,33 @@
 package com.keycloak.test;
 
+import io.restassured.response.Response;
+import org.hamcrest.Matchers;
+import org.json.JSONObject;
+import org.junit.Assert;
+import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
 import static io.restassured.RestAssured.given;
 
 public class LaunchTest {
-    String url="https://keycloak.192.168.64.4.nip.io/";
+    static String url="https://keycloak.192.168.64.11.nip.io/";
+    static String tokenUrl="realms/master/protocol/openid-connect/token";
 
     @Test
     public void validateStatusCode() {
-        given().relaxedHTTPSValidation().when()
-                .get(url).then().statusCode(200);
+        given().relaxedHTTPSValidation()
+            .expect()
+                    .statusCode(200)
+            .when().get(url);
+
+        }
+    @Test
+    public static void  validateWelcomeText() {
+        given().relaxedHTTPSValidation().when().get(url)
+                .then().assertThat()
+                        .body("html.head.title", Matchers.containsString("Welcome to Keycloak"));
 
     }
+
 
 }
